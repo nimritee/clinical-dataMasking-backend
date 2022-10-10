b112676e75b43169f925a263f99085d89fd0a864
Mohit Gureja <mohit.gureja@unity.de>
2022-10-10 20:40:03 +0200

    Changes in helper.py

--- nerm/helper.py
+++ nerm/helper.py
@@ -1,7 +1,7 @@
 from index import ALLOWED_EXTENSIONS
 import os
 from werkzeug.utils import secure_filename
-import logging
+import traceback
 import shutil
 from pathlib import Path
 
@@ -14,29 +14,43 @@ def expected_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def upload(files, upload_directory):
+    directory = upload_directory + "/deploy/"
+    os.makedirs(directory, exist_ok=True)
     for file in files:
         if file and expected_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(upload_directory, filename))
+            file.save(os.path.join(directory, filename))
         else:
             raise ValueError("Exception occurred in finding files with allowed extension!")
-    logging.info("Files are uploaded successfully!")
+    print("Files are uploaded successfully!")
 
-def upload_text(text, upload_folder):
-    with open(os.path.join(upload_folder, MASKING_TEXT_FILE), "w") as text_file:
+def upload_text(text, upload_directory):
+    directory = upload_directory + "/deploy/"
+    os.makedirs(directory, exist_ok=True)
+    with open(os.path.join(directory, MASKING_TEXT_FILE), "w") as text_file:
         text_file.write(text)
 
 def truncate_files(directory):
-    filenames = [ f for f in os.listdir(directory)]
-    for filename in filenames:
-        os.remove(os.path.join(directory, filename))
-    logging.info("Files are deleted successfully!")
+    try:
+        filenames = [ f for f in os.listdir(directory)]
+        for filename in filenames:
+            path = os.path.join(directory, filename)
+            if os.path.isfile(path) or os.path.islink(path):
+                # remove file
+                os.remove(path)
+            elif os.path.isdir(path):
+                # remove directory and all its content
+                shutil.rmtree(path)
+        print("Files are deleted successfully at ", directory)
+    except:
+        traceback.print_exc()
+        print ("Error occurred in deleting the files at ", directory)
 
 def truncate_ann_file(directory):
     filenames = [ f for f in os.listdir(directory) if f.endswith(".ann")]
     for filename in filenames:
         os.remove(os.path.join(directory, filename))
-    logging.info("Files are deleted successfully!")
+    print("Files are deleted successfully!")
 
 def truncate(upload_directory, download_directory, intermediate_directory):
     truncate_files(upload_directory)
